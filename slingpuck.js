c77204146f814632e61ecce1fedd527ee6c2a9ba
Esger Jellema <esgerj@gmail.com>
2020-11-13 21:19:12 +0100

    DistanceJoints

--- slingpuck.js
+++ slingpuck.js
@@ -3,10 +3,10 @@
  * Copyright (c) 2006-2011 Erin Catto  http://www.box2d.org
  */
 
-planck.testbed('Car', function (testbed) {
+planck.testbed('SlingPuck', function (testbed) {
 
     testbed.speed = 10;
-    testbed.hz = 5;
+    testbed.hz = 33;
 
     const pl = planck, Vec2 = pl.Vec2;
     const world = new pl.World({
@@ -14,21 +14,34 @@ planck.testbed('Car', function (testbed) {
     });
 
     const table = world.createBody();
+    const goldenRatio = 1.618;
 
-    const walls = {
+    const wallDef = {
         density: 0.0,
         restitution: 0.6,
         friction: 0.6,
+    };
+
+    const walls = {
         top: 50,
         right: 30,
         bottom: -30,
         left: -30,
         gateTop: 10.0,
         openingLeft: -5.0,
-        openingRight: 5.0
+        openingRight: 5.0,
     };
 
-    const line = (x1, y1, x2, y2, body = walls) => {
+    const wallPositions = [
+        [walls.left, walls.bottom, walls.right, walls.bottom],
+        [walls.right, walls.bottom, walls.right, walls.top],
+        [walls.right, walls.top, walls.left, walls.top],
+        [walls.left, walls.top, walls.left, walls.bottom],
+        [walls.left, walls.gateTop, walls.openingLeft, walls.gateTop],
+        [walls.openingRight, walls.gateTop, walls.right, walls.gateTop]
+    ];
+
+    const line = (x1, y1, x2, y2, body = wallDef) => {
         table.createFixture(pl.Edge(
             Vec2(x1, y1),
             Vec2(x2, y2)),
@@ -36,16 +49,9 @@ planck.testbed('Car', function (testbed) {
         );
     };
 
-    // Walls
-    const drawWals = _ => {
-        line(walls.left, walls.bottom, walls.right, walls.bottom);
-        line(walls.right, walls.bottom, walls.right, walls.top);
-        line(walls.right, walls.top, walls.left, walls.top);
-        line(walls.left, walls.top, walls.left, walls.bottom);
-        line(walls.left, walls.gateTop, walls.openingLeft, walls.gateTop);
-        line(walls.openingRight, walls.gateTop, walls.right, walls.gateTop);
-    };
-    drawWals();
+    wallPositions.forEach(wall => {
+        line(...wall);
+    });
 
     // Puck
     const puckOptions = {
@@ -57,7 +63,7 @@ planck.testbed('Car', function (testbed) {
     const puck = world.createBody({
         type: 'dynamic',
         angularDamping: 5,
-        bullet: true,
+        // bullet: true,
         position: Vec2(1.0, 0.4),
         linearDamping: tableFriction
     });
@@ -68,7 +74,7 @@ planck.testbed('Car', function (testbed) {
 
     // Elastic
     const elasticPart = {
-        width: 0.7,
+        width: 1,
         thickness: 0.125,
         getLeft: i => {
             return elastic.left + elasticPart.width * (i + 1);
@@ -77,7 +83,7 @@ planck.testbed('Car', function (testbed) {
     const elastic = {
         partCount: (walls.right - walls.left) / elasticPart.width - 1,
         left: walls.left,
-        top: -20.0,
+        top: -14.7,
     };
     drawElastic = _ => {
         let prevBody = table;
@@ -85,9 +91,12 @@ planck.testbed('Car', function (testbed) {
             const body = world.createDynamicBody(Vec2(elasticPart.getLeft(i), elastic.top));
             body.createFixture(pl.Box(elasticPart.width / 2, elasticPart.thickness), elastic.top);
 
-            const anchor = Vec2(elastic.left + elasticPart.width * i, elastic.top);
-            world.createJoint(pl.WeldJoint({}, prevBody, body, anchor));
-
+            const anchor = Vec2(elastic.left + elasticPart.width * (i + 0.5), elastic.top);
+            world.createJoint(pl.DistanceJoint({
+                // frequencyHz: 10.0,
+                // dampingRatio: 0.9,
+            }, prevBody, body, anchor));
+            console.log(body.getMass());
             prevBody = body;
         }
         const anchor = Vec2(elastic.left + elasticPart.width * elastic.partCount, elastic.top);
